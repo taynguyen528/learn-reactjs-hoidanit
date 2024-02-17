8559f605945084d98e6909f7c1f217e4e5eb38cb
taynguyen528 <pttnguyen528@gmail.com>
2024-02-17 18:25:37 +0700

    push code new branch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import logo from "./logo.svg";
-import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-
 import React from "react";
+import "./App.scss";
+import Header from "./components/Header/Header";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello Tay Nguyen
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <div>Hello word Tay Nguyen</div>
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p></p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
 
 export default App;
